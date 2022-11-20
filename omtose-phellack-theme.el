62703e077b92a0a43ca825c8f628c765427ccb73
Jonas Bernoulli <jonas@bernoul.li>
2022-11-20 21:28:51 +0100

    Add proper library header to omtose-phellack-theme
    This allows the automated extraction of metadata.

--- omtose-phellack-theme.el
+++ omtose-phellack-theme.el
@@ -1,9 +1,32 @@
-;;; omtose-phellack-theme.el 
-;;; Dark theme, with cold bluish touch. Available in 2 variants.
+;;; omtose-phellack-theme.el --- Two dark themes, with cold blusish touch.
 
-;;; Add theme files under 'variants' subdir to load path.
+;; Copyright (C) 2016 Alexander F. Adhyatma
+
+;; Author: Alexander F. Adhyatma
+;; URL: http:/github.com/franksn/omtose-darker/
+;; Version: 0.2.0
+;; Package-Requires: ((emacs "24"))
+
+;; This file is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary
+
+;; This package provides two dark themes, with cold bluish touch.
+;; These themes are like the illegitimate sons of Gruvbox and Cyberpunk.
 
 ;;; Code:
+
 (let ((dir (file-name-directory load-file-name)))
   (add-to-list 'custom-theme-load-path (concat dir "/variants")))
 
